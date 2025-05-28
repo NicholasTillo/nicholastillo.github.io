6c692a7a94fe3961da6c9840a33e5cac8922f1b4
Nicholas Tillo <nicholastillo@gmail.com>
2025-05-28 00:11:07 -0400

    Tewst

---
+++ my-app/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="component-app">
+        <h1>
+            Nicholas Tillo
+        </h1>
+      </div>
+    );
+  }
+}
