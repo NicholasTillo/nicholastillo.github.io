9c95e8ad4cea58f98c2dd2e3d2c0e38656c2eddb
Nicholas Tillo <nicholastillo@gmail.com>
2025-05-27 22:33:42 -0400

    Raksha

--- WebsiteFiles/my-react-app/src/App.js
+++ WebsiteFiles/my-react-app/src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +14,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hiiiiiiii Raksha I Love you
         </a>
+
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
