b604ef7e76119bbead7fdbea44b0494d8be84e1e
Nicholas Tillo <nicholastillo@gmail.com>
2025-05-27 22:52:21 -0400

    Starting Coding

--- WebsiteFiles/my-react-app/src/App.js
+++ WebsiteFiles/my-react-app/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Nicholas Tillo
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ WebsiteFiles/my-react-app/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Header.css";
+
+
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="component-display">
+        <div>{this.props.value}</div>
+      </div>
+    );
+  }
+}
