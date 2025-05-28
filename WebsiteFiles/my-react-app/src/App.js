fb22199112efcaf9b3ae601506a02bf9146142ae
Nicholas Tillo <nicholastillo@gmail.com>
2025-05-27 22:56:16 -0400

    Revert "Starting Coding"
    This reverts commit b604ef7e76119bbead7fdbea44b0494d8be84e1e.

--- WebsiteFiles/my-react-app/src/App.js
+++ WebsiteFiles/my-react-app/src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Nicholas Tillo
-        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
